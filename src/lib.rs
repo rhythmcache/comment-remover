1edf75cf92d7e174858a560211c22868269febb2
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-10-16 20:14:30 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod c_type;
 pub mod shell_type;
+pub mod xml_type;
 
 pub use c_type::remove_c_type_comments;
 pub use shell_type::{remove_hash_comments_basic, remove_python_comments, remove_shell_comments};
+pub use xml_type::xml_type_remover;
